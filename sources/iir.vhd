92ed35550beafc2439fd0dc3c34cc08d78c565a1
BingQZh <gracebqz@gmail.com>
2022-03-10 19:39:43 -0800

    updates

--- sources/iir.vhd
+++ sources/iir.vhd
@@ -32,8 +32,8 @@ end iir;
 
 architecture rtl of iir is
     -- registers capturing previous values
-    signal x_1, x_2, y_1, y_2 : std_logic_vector(bit_width-1 downto 0) := (others => '0');
-
+    signal x_1, x_2, y_0, y_1, y_2 : signed(width_internal-1 downto 0) := (others => '0');
+    signal y64 : signed( (width_internal*2)-1 downto 0) := (others => '0');
     -- results from multiplying the gains
     signal prod_a0, prod_a1, prod_a2, prod_b1, prod_b2 : signed(width_internal-1 downto 0) := (others => '0');
 begin
@@ -45,72 +45,35 @@ begin
             if rst = '0' then
                 x_1 <= (others => '0');
                 x_2 <= (others => '0');
+                y_1 <= (others => '0');
                 y_2 <= (others => '0');
             else
                 if valid = '1' then
                     -- at each rising edge, the signals will be updated with previous values
-                    x_1 <= x;
+                    x_1 <= resize(signed(x), width_internal);
                     x_2 <= x_1;
                     --y_1 <= (prod_a0 + prod_a1 + prod_a2) - (prod_b1 + prod_b2);
-                    y_1 <= std_logic_vector(resize((prod_a0 + prod_a1 + prod_a2) - (prod_b1 + prod_b2), bit_width));
+--                    y_1 <= std_logic_vector(resize((prod_a0 + prod_a1 + prod_a2) - (prod_b1 + prod_b2), bit_width));
+                    y_1 <= resize(y64, width_internal);
                     y_2 <= y_1;
                 end if;
             end if;
         end if;
     end process;
 
-    IIR_FILTER: process(valid)
-    begin
-        if valid = '1' then
+ y64 <= shift_right(
+            (
+                (resize(signed(x), width_internal) * to_signed(a0, width_internal)) -- x * a0
+                + (x_1 * to_signed(a1, width_internal)) -- x_1 * a1
+                + (x_2 * to_signed(a2, width_internal)) -- x_2 * a2
+            ) - ( -- subtracting the feedback network (below) from the feedforward network (above)
+                (y_1 * to_signed(b1, width_internal)) -- y_1 * b1
+                + (y_2 * to_signed(b2, width_internal)) -- y_2 * b2
+            ), width_internal-2); 
 
-                y <= std_logic_vector(resize(((resize(shift_right(resize(signed(x), width_internal) * to_signed(a0, width_internal), width_internal-2), width_internal)) + prod_a1 + prod_a2) - (prod_b1 + prod_b2), bit_width));
-
-                prod_a0 <= resize(shift_right(resize(signed(x), width_internal) * to_signed(a0, width_internal), width_internal-2), width_internal);
---        else
---            y <= (others => '0');
---            prod_a0 <= (others => '0');
-        
-        end if;
-    end process;
-
-    A1_PROC: process(rst, x_1)
-    begin
-        if rst = '0' then
-            prod_a1 <= (others => '0');
-        else
-            prod_a1 <= resize(shift_right(resize(signed(x_1), width_internal) * to_signed(a1, width_internal), width_internal-2), width_internal);
-        end if;
-    end process;
-
-    A2_PROC: process(rst, x_2)
-    begin
-        if rst = '0' then
-            prod_a2 <= (others => '0');
-        else
-            prod_a2 <= resize(shift_right(resize(signed(x_2), width_internal) * to_signed(a2, width_internal), width_internal-2), width_internal);
-        end if;
-    end process;
-
-    B1_PROC: process(rst, y_1)
-    begin
-        if rst = '0' then
-            prod_b1 <= (others => '0');
-        else
-            prod_b1 <= resize(shift_right(resize(signed(y_1), width_internal) * to_signed(b1, width_internal), width_internal-2), width_internal);    
-        end if;
-    end process;
-
-    B2_PROC: process(rst, y_2)
-    begin
-        if rst = '0' then
-            prod_b2 <= (others => '0');
-        else
-            prod_b2 <= resize(shift_right(resize(signed(y_2), width_internal) * to_signed(b2, width_internal), width_internal-2), width_internal);
-        end if;
-    end process;
+y <= std_logic_vector(resize(y64, bit_width));
 
 end architecture;
-
 -- lowpass filter values        Q2.30:
 -- a0 = 0.003916123487156427    4204905
 -- a1 = 0.007832246974312854    8409811
